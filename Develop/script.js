32787c0f755ad168c30c6b2b04d424d8a8806d7a
Chris Wilson <chrisw@myself.com>
2023-02-03 17:05:00 -0500

    code functional, HTML added

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,9 @@
-// TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 $(".saveBtn").on("click", function () {
   var key = $(this).parent().attr("id").split("-")[1];
   var value = $(this).parent().find(".description").val();
   localStorage.setItem(key, value);
 });
-// TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
 var currentHour = dayjs().hour();
 $(".time-block").each(function () {
   var blockHour = $(this).attr("id").split("-")[1];
@@ -27,7 +18,7 @@ $(".time-block").each(function () {
     $(this).find(".description").addClass("future");
   }
 });
-// TODO: Add code to display the current date in the header of the page.
+
 var currentDay = $("#currentDay");
 currentDay.text(dayjs().format('dddd, MMM D YYYY'));
  
